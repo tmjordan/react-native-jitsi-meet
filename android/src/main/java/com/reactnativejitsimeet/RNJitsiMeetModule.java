fe6fe9daad96508cd3a53efeb253cd04a71ab19a
tmjordan <jordantchokonte@yahoo.fr>
2020-06-14 13:00:23 +0100

    Update RNJitsiMeetModule.java

--- android/src/main/java/com/reactnativejitsimeet/RNJitsiMeetModule.java
+++ android/src/main/java/com/reactnativejitsimeet/RNJitsiMeetModule.java
@@ -57,6 +57,13 @@ public class RNJitsiMeetModule extends ReactContextBaseJavaModule {
                             .setRoom(url)
                             .setAudioOnly(false)
                             .setUserInfo(_userInfo)
+                            .setFeatureFlag("add-people.enabled", false)
+                            .setFeatureFlag("chat.enabled", false)
+                            .setFeatureFlag("live-streaming.enabled", false)
+                            .setFeatureFlag("meeting-name.enabled", false)
+                            .setFeatureFlag("meeting-password.enabled", false)
+                            .setFeatureFlag("recording.enabled", false)
+                            .setFeatureFlag("invite.enabled", false)
                             .build();
                     mJitsiMeetViewReference.getJitsiMeetView().join(options);
                 }
